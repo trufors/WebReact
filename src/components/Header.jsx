c7de4e6f00b287a406853e78d9f7a6b18178375b
Vlad Glatkov <vladglatkov@192.168.1.3>
2022-06-27 17:36:09 +0300

    components start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+      <header className="header">
+        <img src="https://w7.pngwing.com/pngs/70/772/png-transparent-vimeo-computer-icons-logo-short-film-vitag-svg-miscellaneous-television-text-thumbnail.png" alt='logo' />
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <img src="https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg" alt='' />
+        </div>
+    )
+}
+
+export default Profile;
